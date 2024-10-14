b57912e2238d2cbc6e99f2cc7088b1d3a941eb7a
Egurazdov <egurazdov@yandex.ru>
2024-10-14 21:08:29 +0300

    first

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,20 @@
+const val PI = 3.14
 fun main() {
-    println("Hello World!")
+    val sideA = 8
+    val sideB = 11
+    val perimeter = 4 * sideA
+    val square = sideA * sideA
+    val perimeterRec = 2 * (sideA + sideB)
+    val squareRec = sideA * sideB
+    val diameter = 98
+    val lenght = PI * diameter
+    val l = 345
+    val lenghtInM = l / 100
+    val a = 456
+    val b = 19
+    val c = a / b
+
+    println(
+        "Psq = " + perimeter + " | Ssq = " + square + " | Prec = " + perimeterRec + " | Srec = " + squareRec + " | Lcir=" + lenght + " | L=" + lenghtInM + " | A=" + c
+    )
 }
